ae8b726fedb88459add154ca4add99948f79cc50
danellecline <dcline@mbari.org>
2024-03-11 16:52:05 -0700

    fix: added missing config.ini default

--- sdcat/common_args.py
+++ sdcat/common_args.py
@@ -9,4 +9,5 @@ from sdcat.config.config import default_config_ini
 # Common arguments for processing commands
 config_ini = click.option('--config-ini',
                           type=str,
+                          default=default_config_ini,
                           help=f'Path to config file to override. Defaults are in {default_config_ini}. Copy to your own custom.ini file to override')
