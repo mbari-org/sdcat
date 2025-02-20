23cf120a5d3435af99f402252a2cc4e56a30fbae
danellecline <dcline@mbari.org>
2025-02-20 15:16:38 -0800

    fix: only capture top 2 classes and scores

--- sdcat/cluster/embedding.py
+++ sdcat/cluster/embedding.py
@@ -53,14 +53,13 @@ class ViTWrapper:
             logits = outputs.logits
             embeddings = self.model.base_model(**inputs)
             batch_embeddings = embeddings.last_hidden_state[:, 0, :].cpu().numpy()
-            # Get the top classes and scores
-            if len(logits.shape) == 1:
-                top_scores, top_classes = torch.topk(logits, 1)
-            else:
-                top_scores, top_classes = torch.topk(logits, 3)
-            top_classes = top_classes.cpu().numpy()
+            # Get up to the top two classes and scores
+            top_n = min(logits.shape[1], 2)
+            top_scores, top_classes = torch.topk(logits, top_n)
             top_scores = F.softmax(top_scores, dim=-1).cpu().numpy()
-            predicted_classes = [",".join([self.model.config.id2label[class_idx] for class_idx in class_list]) for class_list in top_classes]
+            top_classes = top_classes.cpu().numpy()
+            predicted_classes = [",".join([self.model.config.id2label[class_idx] for class_idx in class_list]) for
+                                 class_list in top_classes]
             predicted_scores = [",".join([str(score) for score in score_list]) for score_list in top_scores]
 
         return batch_embeddings, predicted_classes, predicted_scores
