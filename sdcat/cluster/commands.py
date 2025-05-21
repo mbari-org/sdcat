e6092051ab84bba18d191cc956af15223ca4c0aa
danellecline <dcline@mbari.org>
2025-05-21 16:49:08 -0700

    removed redundant area filter

--- sdcat/cluster/commands.py
+++ sdcat/cluster/commands.py
@@ -137,13 +137,6 @@ def run_cluster_det(det_dir, save_dir, device, use_vits, weighted_score, config_
         df['score'] = 0.
         df['score_s'] = 0.
 
-        # Remove small or large detections before clustering
-        size_before = len(df)
-        info(f'Searching through {size_before} detections')
-        df = df[(df['area'] > min_area) & (df['area'] < max_area)]
-        size_after = len(df)
-        info(f'Removed {size_before - size_after} detections that were too large or too small')
-
         def within_1_percent_of_corners(row):
             threshold = 0.01  # 1% threshold
 
